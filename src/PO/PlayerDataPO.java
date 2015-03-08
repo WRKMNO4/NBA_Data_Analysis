32c301b12de4b66187680e191c18a12e95e636f9
shengxiaoyu <shengxiaoyu>
2015-03-08 21:53:45 +0800

    Designing POs

--- src/PO/PlayerDataPO.java
+++ src/PO/PlayerDataPO.java
@@ -1,5 +1,125 @@
 package PO;
 
 public class PlayerDataPO {
+	TeamPO team ;
+	int numberOfMatch ;//参赛场数
+	int numberOfStarting ;//首发场数
+	int numberOfRebound ;//篮板数
+	int numberOfAssist ;//助攻数
+	
+	double presentTime ;//在场时间
+	double percentageOfShooting ;//投篮命中率
+	double percentageOf3_Point ;//三分命中率
+	double percentageOffreeThrow ;//罚球命中率
+	
+	int numberOfAttack ;//进攻数
+	int numberOfDefense ;//防守数
+	int numberOfSteal ;//抢断数
+	int numberOfBlock ;//盖帽数 
+	int numberOfFault ;//失误数
+	int numberOfFoul ;//犯规数，
+	int score ;//得分，
+	int efficiency ;//效率，
+	
+	double efficiencyOfGmSc ;//GmSc效率值，
+	double percentageOfTrueShooting ;//真实命中率，
+	double efficiencyOfShooting ;//投篮效率，
+	double percentageOfRebound ; //篮板率，
+	double percentageOfAttackingRebound ;//进攻篮板率，
+	double percentageOfDefenseRebound ;//防守篮板率，
+	double percentageOfAssist ;// 助攻率，
+	double percentageOfSteal ;//抢断率，
+	double percentageOfBlock ;//盖帽率，
+	double percentageOfFault ;//失误率，
+	double percentageOfUse ;//使用率
+	
+	
+	
+	
+	public TeamPO getTeam() {
+		return team;
+	}
+	public int getNumberOfMatch() {
+		return numberOfMatch;
+	}
+	public int getNumberOfStarting() {
+		return numberOfStarting;
+	}
+	public int getNumberOfRebound() {
+		return numberOfRebound;
+	}
+	public int getNumberOfAssist() {
+		return numberOfAssist;
+	}
+	public double getPresentTime() {
+		return presentTime;
+	}
+	public double getPercentageOfShooting() {
+		return percentageOfShooting;
+	}
+	public double getPercentageOf3_Point() {
+		return percentageOf3_Point;
+	}
+	public double getPercentageOffreeThrow() {
+		return percentageOffreeThrow;
+	}
+	public int getNumberOfAttack() {
+		return numberOfAttack;
+	}
+	public int getNumberOfDefense() {
+		return numberOfDefense;
+	}
+	public int getNumberOfSteal() {
+		return numberOfSteal;
+	}
+	public int getNumberOfBlock() {
+		return numberOfBlock;
+	}
+	public int getNumberOfFault() {
+		return numberOfFault;
+	}
+	public int getNumberOfFoul() {
+		return numberOfFoul;
+	}
+	public int getScore() {
+		return score;
+	}
+	public int getEfficiency() {
+		return efficiency;
+	}
+	public double getEfficiencyOfGmSc() {
+		return efficiencyOfGmSc;
+	}
+	public double getPercentageOfTrueShooting() {
+		return percentageOfTrueShooting;
+	}
+	public double getEfficiencyOfShooting() {
+		return efficiencyOfShooting;
+	}
+	public double getPercentageOfRebound() {
+		return percentageOfRebound;
+	}
+	public double getPercentageOfAttackingRebound() {
+		return percentageOfAttackingRebound;
+	}
+	public double getPercentageOfDefenseRebound() {
+		return percentageOfDefenseRebound;
+	}
+	public double getPercentageOfAssist() {
+		return percentageOfAssist;
+	}
+	public double getPercentageOfSteal() {
+		return percentageOfSteal;
+	}
+	public double getPercentageOfBlock() {
+		return percentageOfBlock;
+	}
+	public double getPercentageOfFault() {
+		return percentageOfFault;
+	}
+	public double getPercentageOfUse() {
+		return percentageOfUse;
+	}
+	
 	
 }
--- src/PO/TeamDataPO.java
+++ src/PO/TeamDataPO.java
@@ -1,5 +1,115 @@
 package PO;
 
 public class TeamDataPO {
-
+	int numberOfMatch ;//比赛场数
+	int numberOfShooting ;//投篮命中数
+	int numberOfShotAttempt ;//，投篮出手数，
+	int numberOf3_point ;//三分命中数，
+	int numberOf3_pointAttempt ;//三分出手数，
+	int numberOfFreeThrow ;//罚球命中数，
+	int numberOfFreeThrowAttempt ;//罚球出手数，
+	int numberOfAttackingRebound ;//进攻篮板数，
+	int numberOfDefenseRebound ;//防守篮板数，
+	int numberOfRebound ;//篮板球，
+	int numberOfAssist ;//助攻数，
+	int numberOfSteal ;//抢断数，
+	int numberOfBlock ;//盖帽数，
+	int numberOfFault ;//失误数
+	int numberOfFoul ;//，犯规数，
+	int score ;//比赛得分，
+	
+	double percentageOfShooting ;//投篮命中率，
+	double percentageOf3_point ;//三分命中率，
+	double percentageOfFreeThrow ;//罚球命中率，
+	double percentageOfWinning ;//胜率，
+	double roundOfAttack ;//进攻回合
+	double efficiencyOfAttack ;//，进攻效率，
+	double efficiencyOfDefense ;//防守效率，
+	double efficiencyOfBlock ;//篮板效率，
+	double efficiencyOfSteal ;//抢断效率
+	double efficiencyOfAssist ;//助攻率。
+	
+	
+	
+	public int getNumberOfMatch() {
+		return numberOfMatch;
+	}
+	public int getNumberOfShooting() {
+		return numberOfShooting;
+	}
+	public int getNumberOfShotAttempt() {
+		return numberOfShotAttempt;
+	}
+	public int getNumberOf3_point() {
+		return numberOf3_point;
+	}
+	public int getNumberOf3_pointAttempt() {
+		return numberOf3_pointAttempt;
+	}
+	public int getNumberOfFreeThrow() {
+		return numberOfFreeThrow;
+	}
+	public int getNumberOfFreeThrowAttempt() {
+		return numberOfFreeThrowAttempt;
+	}
+	public int getNumberOfAttackingRebound() {
+		return numberOfAttackingRebound;
+	}
+	public int getNumberOfDefenseRebound() {
+		return numberOfDefenseRebound;
+	}
+	public int getNumberOfRebound() {
+		return numberOfRebound;
+	}
+	public int getNumberOfAssist() {
+		return numberOfAssist;
+	}
+	public int getNumberOfSteal() {
+		return numberOfSteal;
+	}
+	public int getNumberOfBlock() {
+		return numberOfBlock;
+	}
+	public int getNumberOfFault() {
+		return numberOfFault;
+	}
+	public int getNumberOfFoul() {
+		return numberOfFoul;
+	}
+	public int getScore() {
+		return score;
+	}
+	public double getPercentageOfShooting() {
+		return percentageOfShooting;
+	}
+	public double getPercentageOf3_point() {
+		return percentageOf3_point;
+	}
+	public double getPercentageOfFreeThrow() {
+		return percentageOfFreeThrow;
+	}
+	public double getPercentageOfWinning() {
+		return percentageOfWinning;
+	}
+	public double getRoundOfAttack() {
+		return roundOfAttack;
+	}
+	public double getEfficiencyOfAttack() {
+		return efficiencyOfAttack;
+	}
+	public double getEfficiencyOfDefense() {
+		return efficiencyOfDefense;
+	}
+	public double getEfficiencyOfBlock() {
+		return efficiencyOfBlock;
+	}
+	public double getEfficiencyOfSteal() {
+		return efficiencyOfSteal;
+	}
+	public double getEfficiencyOfAssist() {
+		return efficiencyOfAssist;
+	}
+	
+	
+	
 }
