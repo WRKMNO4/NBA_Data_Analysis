1fc9b7b5b252b3d94458c64cb5d0d29ef5b034a4
Bagnogetoff <1051943988@qq.com>
2015-03-19 19:00:41 +0800

    .

--- src/com/kmno4/presentation/PlayerDetailFrame.java
+++ src/com/kmno4/presentation/PlayerDetailFrame.java
@@ -36,16 +36,12 @@ public class PlayerDetailFrame extends JFrame {
 	public PlayerDetailFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, Config.PLAYER_DETAIL_UI_WIDTH, Config.PLAYER_DETATI_UI_TOP_HEIGHT);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-		this.setVisible(true);
-		this.setLayout(null);
+		setLayout(null);
 		
 		playerDetailPanel = new PlayerDetailPanel();
 		this.add(playerDetailPanel);
 		
+		setVisible(true);
 	}
 
 }
