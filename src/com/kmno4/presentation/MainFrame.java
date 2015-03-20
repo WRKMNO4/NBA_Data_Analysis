2e57bfc150483e6f59fb54c52634bb79b9d3c2a1
gaoyangyiqiao <gyyq1994@gmail.com>
2015-03-20 18:50:07 +0800

    fucmian

--- src/com/kmno4/presentation/MainFrame.java
+++ src/com/kmno4/presentation/MainFrame.java
@@ -88,7 +88,7 @@ public class MainFrame extends JFrame implements MouseListener{
 		});
 		
 		mainUI.start();
-		splash.start();
+//		splash.start();
 
 	}
 
--- src/main/FucMain.java
+++ src/main/FucMain.java
@@ -1,87 +1,77 @@
 package main;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 import com.kmno4.common.Config;
+import com.kmno4.presentation.MainFrame;
 import com.kmno4.presentation.SplashPanel;
 
 public class FucMain {
 	
-	class SplashPaneThread extends Thread{
-		@Override
-		public void run() {
-		// TODO Auto-generated method stub
-			//执行5秒
-			long begin_time=System.currentTimeMillis();
-			
-				try {
-					JFrame GFrame = new JFrame();	//创建窗口
-					SplashPanel DPanel = new SplashPanel();	//创建画板
-					/*设置JFrame*/
-					GFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-					GFrame.setLocation(100, 100);
-					GFrame.setSize(Config.UI_WIDTH, Config.UI_HEIGHT);
-					GFrame.setVisible(true);
-					GFrame.add(DPanel);	//在JFrame中加入DPanel
-					DPanel.launch();					
-					while(true){
-						if(System.currentTimeMillis()-begin_time>3000){
-							GFrame.dispose();
-							break;
-						}
-					}
-					
-					stop();
-					
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			
-		}
-	}
+	
 
 	public static void main(String[] args){
-		new Thread(new Runnable(){
+		Thread splash=new Thread(new Runnable(){
+			@Override
+			public void run() {
+				long begin_time=System.currentTimeMillis();
+				JFrame GFrame = new JFrame();	//创建窗口
+				SplashPanel DPanel = new SplashPanel();	//创建画板
+				/*设置JFrame*/
+				GFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				GFrame.setUndecorated(true);
+				Toolkit kit=Toolkit.getDefaultToolkit();
+				Dimension screensize=kit.getScreenSize();
+				int screenheight=screensize.height;
+				int screenwidth=screensize.width;
+				GFrame.setLocation(screenwidth/8,screenheight/8);
+				GFrame.setSize(Config.UI_WIDTH, Config.UI_HEIGHT);
+				GFrame.setVisible(true);
+				GFrame.add(DPanel);	//在JFrame中加入DPanel
+				DPanel.launch();					
+				while(true){
+					if(System.currentTimeMillis()-begin_time>7000){
+						GFrame.dispose();
+						break;
+					}				
+				}				
+			}			
+		});
+		
+		Thread mainUI=new Thread(new Runnable(){
 
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
-				//执行m秒
 				long begin_time=System.currentTimeMillis();
 				
-					try {
-						JFrame GFrame = new JFrame();	//创建窗口
-						SplashPanel DPanel = new SplashPanel();	//创建画板
-						/*设置JFrame*/
-						GFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-						GFrame.setLocation(100, 100);
-						GFrame.setSize(Config.UI_WIDTH, Config.UI_HEIGHT);
-						GFrame.setVisible(true);
-						GFrame.add(DPanel);	//在JFrame中加入DPanel
-						DPanel.launch();					
-						while(true){
-							if(System.currentTimeMillis()-begin_time>7000){
-								GFrame.dispose();
-								break;
-							}
-						
-						}
-					} catch (Exception e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+				MainFrame frame = new MainFrame();
+				frame.setLayout(null);
+				frame.setResizable(false);
+				//获取屏幕宽高
+				Toolkit kit=Toolkit.getDefaultToolkit();
+				Dimension screensize=kit.getScreenSize();
+				int screenheight=screensize.height;
+				int screenwidth=screensize.width;
+				
+				frame.setSize(Config.UI_WIDTH,Config.UI_HEIGHT);
+				frame.setLocation(screenwidth/8,screenheight/8);				
+				MainFrame.playerSelectionPanel.setBounds(0, Config.TOP_TAB_HEIGHT+Config.INTRODUCTION_WHITE, 
+			Config.UI_WIDTH, Config.SELECTION_HEIGHT);
+				
+				while(true){
+					if((System.currentTimeMillis()-begin_time)>7000){
+						frame.setVisible(true);
+						break;
 					}
+				}
 			}			
-		}).start();
-//		JFrame GFrame = new JFrame();	//创建窗口
-//		GFrame.setUndecorated(true);
-//		SplashPanel DPanel = new SplashPanel();	//创建画板
-//		/*设置JFrame*/
-//		GFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		GFrame.setLocation(100, 100);
-//		GFrame.setSize(Config.UI_WIDTH, Config.UI_HEIGHT);
-//		GFrame.setVisible(true);
-//		GFrame.add(DPanel);	//在JFrame中加入DPanel
-//		DPanel.launch();
+		});
+		
+		mainUI.start();
+		splash.start();
 	}
 }
