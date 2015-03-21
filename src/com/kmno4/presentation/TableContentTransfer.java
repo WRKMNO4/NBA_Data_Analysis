b5e08ce706f7eff3d91ed8909553cd9db38962c9
gaoyangyiqiao <gyyq1994@gmail.com>
2015-03-21 20:02:32 +0800

    transfer

--- src/com/kmno4/presentation/TableContentTransfer.java
+++ src/com/kmno4/presentation/TableContentTransfer.java
@@ -6,12 +6,10 @@ import PO.PlayerPO;
 
 public class TableContentTransfer {
 
-	public void transfer(String[] playerHead,List<PlayerPO> players){
-		String[][] body=new String[players.size()][playerHead.length];
+	public String[][] transfer(int cloums,List<PlayerPO> players){
+		String[][] body=new String[players.size()][cloums];
 		for(int i=0;i<players.size();i++){
-			for(int j=0;j<playerHead.length;j++){
-				String[i][j]=players.get(i).getAttri(playerHead[j]);
-			}
+			
 		}		
 	}
 }
