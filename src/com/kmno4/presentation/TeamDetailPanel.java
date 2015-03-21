919c53029e86ab8da477a6c4c1f2a729f47826d3
Bagnogetoff <1051943988@qq.com>
2015-03-21 18:01:04 +0800

    z

--- src/com/kmno4/presentation/TeamDetailPanel.java
+++ src/com/kmno4/presentation/TeamDetailPanel.java
@@ -8,13 +8,19 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import com.kmno4.common.Config;
+import com.kmno4.presentation.button.BorderLabel;
 import com.kmno4.presentation.table.Table;
 
 @SuppressWarnings("serial")
 public class TeamDetailPanel extends JPanel {
 	private TeamDetailPanel teamDetailPanel = this;
 	private JLabel
-	    team_icon;
+	    team_icon,
+	    team_name,
+	    team_coach,
+	    team_achi,
+	    avg,
+	    sum;
 	private Table
 	    mainInfo,
 	    sumInfo,
@@ -29,6 +35,99 @@ public class TeamDetailPanel extends JPanel {
 		layout = new GridBagLayout();
 		setLayout(layout);
 		c = new GridBagConstraints();
+		
+		team_icon = new BorderLabel("队伍头像", JLabel.CENTER);
+		c.gridx = 0;
+		c.gridy = 0;
+		c.gridwidth = 1;
+		c.gridheight = 3;
+		c.weightx = 4;
+		c.weighty = 4;
+		c.fill = GridBagConstraints.BOTH;
+		layout.setConstraints(team_icon, c);
+		add(team_icon);
+		
+		team_name = new BorderLabel("", JLabel.CENTER);
+		c.gridx = 1;
+		c.gridy = 0;
+		c.gridwidth = 2;
+		c.gridheight = 1;
+		c.weightx = 3;
+		c.weighty = 2;
+		c.fill = GridBagConstraints.BOTH;
+		layout.setConstraints(team_name, c);
+		add(team_name);
+		
+		team_coach = new BorderLabel("", JLabel.CENTER);
+		c.gridx = 1;
+		c.gridy = 1;
+		c.gridwidth = 2;
+		c.gridheight = 1;
+		c.weightx = 3;
+		c.weighty = 1;
+		c.fill = GridBagConstraints.BOTH;
+		layout.setConstraints(team_coach, c);
+		add(team_coach);
+		
+		team_achi = new BorderLabel("", JLabel.CENTER);
+		c.gridx = 3;
+		c.gridy = 0;
+		c.gridwidth = 1;
+		c.gridheight = 1;
+		c.weightx = 3;
+		c.weighty = 2;
+		c.fill = GridBagConstraints.BOTH;
+		layout.setConstraints(team_achi, c);
+		add(team_achi);
+		
+		sum = new BorderLabel();
+		c.gridx = 4;
+		c.gridy = 2;
+		c.gridwidth = 1;
+		c.gridheight = 1;
+		c.weightx = 1;
+		c.weighty = 1;
+		c.fill = GridBagConstraints.BOTH;
+		layout.setConstraints(sum, c);
+		add(sum);
+		
+		avg = new BorderLabel();
+		c.gridx = 5;
+		layout.setConstraints(avg, c);
+		add(avg);
+		
+		mainInfo = new Table(
+				new String[]{},
+				new String[][]{});
+		c.gridx = 1;
+		c.gridy = 2;
+		c.gridwidth = 3;
+		c.gridheight = 1;
+		c.weightx = 6;
+		c.weighty = 1;
+		c.fill = GridBagConstraints.BOTH;
+		layout.setConstraints(mainInfo, c);
+		add(mainInfo);
+		
+		sumInfo = new Table(
+				new String[]{},
+				new String[][]{});
+		c.gridx = 0;
+		c.gridy = 4;
+		c.gridwidth = 5;
+		c.gridheight = 1;
+		c.weightx = 12;
+		c.weighty = 3;
+		c.fill = GridBagConstraints.BOTH;
+		layout.setConstraints(sumInfo, c);
+		add(sumInfo);
+		
+		avgInfo = new Table(
+				new String[]{},
+				new String[][]{});
+		layout.setConstraints(avgInfo, c);
+		add(avgInfo);
+		
 	}
 	
 	
