6f2796e1523ee3736c777395a6bb354ffe7e7213
Bagnogetoff <1051943988@qq.com>
2015-03-25 00:03:42 +0800

    晕 有点麻烦

--- src/com/kmno4/presentation/table/SlideTable.java
+++ src/com/kmno4/presentation/table/SlideTable.java
@@ -1,19 +1,26 @@
 package com.kmno4.presentation.table;
 
-import javax.swing.JPanel;
-
 /**
  * 
  * @author hutao
  *
  */
 @SuppressWarnings("serial")
-public class SlideTable extends JPanel {
-	private SmallTable table;
-	
+public class SlideTable extends SmallTable {
 	public SlideTable(String[] headStr, String[][] bodyString) {
-		table = new SmallTable(headStr, bodyString);
+		super(headStr, bodyString);
 	}
 
+	//private SmallTable table;
+	/*
+	public SlideTable(String[] headStr, String[][] bodyString) {
+		this.setLayout(null);
+		table = new SmallTable(headStr, bodyString);
+		add(table);
+		table.setLocation(20, 20);
+		table.setSize(this.getWidth(), this.getHeight());
+		System.out.println();
+	}
+	*/
 }
 
