0135fdb448610b99f826484eb9a59090ef4151d6
gaoyangyiqiao <gyyq1994@gmail.com>
2015-04-10 23:55:08 +0800

    x

--- src/com/kmno4/presentation/HotSelectionPanel.java
+++ src/com/kmno4/presentation/HotSelectionPanel.java
@@ -6,6 +6,8 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 import com.kmno4.common.Config;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
 
 public class HotSelectionPanel extends JPanel {
 
@@ -17,6 +19,35 @@ public class HotSelectionPanel extends JPanel {
 		this.setBounds(0, 0, Config.UI_WIDTH, Config.MATCH_SELECTION_PANEL_HEIGHT);
 		this.setVisible(true);
 		this.setBackground(Color.gray);
+		
+		JLabel lb_data_player = new JLabel("当日热点球员");
+		lb_data_player.setBounds(0, 0, 200, 30);
+		add(lb_data_player);
+		JLabel lb_season_player = new JLabel("赛季热点球员");
+		lb_season_player.setBounds(200, 0, 200, 30);
+		add(lb_season_player);
+		JLabel lb_improve_player = new JLabel("进步最快球员");
+		lb_improve_player.setBounds(400, 0, 200, 30);
+		add(lb_improve_player);
+		JLabel lb_season_team = new JLabel("赛季热点球队");
+		lb_season_team.setBounds(600, 0, 200, 30);
+		add(lb_season_team);
+		
+		JRadioButton radioButton = new JRadioButton("第一赛季");
+		radioButton.setBounds(0, 35, 141, 23);
+		add(radioButton);
+		
+		JRadioButton radioButton_1 = new JRadioButton("第二赛季");
+		radioButton_1.setBounds(0, 61, 141, 23);
+		add(radioButton_1);
+		
+		JRadioButton radioButton_2 = new JRadioButton("第三赛季");
+		radioButton_2.setBounds(0, 85, 141, 23);
+		add(radioButton_2);
+		
+		JRadioButton radioButton_3 = new JRadioButton("第四赛季");
+		radioButton_3.setBounds(0, 106, 141, 23);
+		add(radioButton_3);
 		
 		
 	}
--- src/com/kmno4/presentation/MatchSelectionPanel.java
+++ src/com/kmno4/presentation/MatchSelectionPanel.java
@@ -11,7 +11,6 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 public class MatchSelectionPanel extends JPanel {
-	private JTextField textField;
 
 	/**
 	 * Create the panel.
@@ -27,24 +26,31 @@ public class MatchSelectionPanel extends JPanel {
 		comboBox.setBounds(22, 75, 186, 27);
 		add(comboBox);
 		
-		JLabel lblNewLabel = new JLabel("搜索");
-		lblNewLabel.setBounds(22, 22, 42, 16);
-		add(lblNewLabel);
+		JLabel lb_firstSeason = new JLabel("第一赛季");
+		lb_firstSeason.setBounds(0, 0, 200, 30);
+		add(lb_firstSeason);
+		JLabel lb_secondSeason = new JLabel("第二赛季");
+		lb_secondSeason.setBounds(200,0, 200, 30);
+		add(lb_secondSeason);
+		JLabel lb_thirdSeason = new JLabel("第三赛季");
+		lb_thirdSeason.setBounds(400,0, 200, 30);
+		add(lb_thirdSeason);
+		JLabel lb_forthSeason = new JLabel("第四赛季");
+		lb_forthSeason.setBounds(600,0, 200, 30);
+		add(lb_forthSeason);
 		
 		JLabel lblNewLabel_1 = new JLabel("日历");
 		lblNewLabel_1.setIcon(Config.SCHEDULE);
 		lblNewLabel_1.setBounds(660, 66, 98, 43);
 		add(lblNewLabel_1);
 		
-
-		textField = new JTextField();
-		textField.setBounds(63, 16, 134, 28);
-		add(textField);
-		textField.setColumns(10);
-		
 		JComboBox comboBox_1 = new JComboBox();
 		comboBox_1.setBounds(275, 75, 164, 27);
 		add(comboBox_1);
+		
+		JLabel lblVs = new JLabel("VS");
+		lblVs.setBounds(213, 79, 61, 16);
+		add(lblVs);
 	}
 	
 	
