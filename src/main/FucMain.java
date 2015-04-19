af031cb78229b7d166698c816678a208de8e8e53
gaoyangyiqiao <gyyq1994@gmail.com>
2015-04-20 00:11:55 +0800

    第二个界面库

--- src/main/FucMain.java
+++ src/main/FucMain.java
@@ -3,9 +3,18 @@ package main;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+
+import org.jvnet.substance.SubstanceLookAndFeel;
+import org.jvnet.substance.border.StandardBorderPainter;
+import org.jvnet.substance.button.ClassicButtonShaper;
+import org.jvnet.substance.painter.StandardGradientPainter;
+import org.jvnet.substance.skin.EmeraldDuskSkin;
+import org.jvnet.substance.theme.SubstanceTerracottaTheme;
+import org.jvnet.substance.title.FlatTitlePainter;
+import org.jvnet.substance.watermark.SubstanceBubblesWatermark;
 
 import com.alee.laf.WebLookAndFeel;
 import com.kmno4.common.Config;
@@ -13,70 +22,91 @@ import com.kmno4.presentation.MainFrame;
 import com.kmno4.presentation.SplashPanel;
 
 public class FucMain {
-	
-	
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
+		// 组件
 		WebLookAndFeel.install();
-		
-		Thread splash=new Thread(new Runnable(){
+		// look$feel——————second choice
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		JDialog.setDefaultLookAndFeelDecorated(true);
+//		try {
+//			UIManager.setLookAndFeel(new SubstanceLookAndFeel());
+//			JFrame.setDefaultLookAndFeelDecorated(true);
+//			JDialog.setDefaultLookAndFeelDecorated(true);
+//			SubstanceLookAndFeel
+//					.setCurrentTheme(new SubstanceTerracottaTheme());
+//			SubstanceLookAndFeel.setSkin(new EmeraldDuskSkin());
+//			SubstanceLookAndFeel
+//					.setCurrentButtonShaper(new ClassicButtonShaper());
+//			SubstanceLookAndFeel
+//					.setCurrentWatermark(new SubstanceBubblesWatermark());
+//			SubstanceLookAndFeel
+//					.setCurrentBorderPainter(new StandardBorderPainter());
+//			SubstanceLookAndFeel
+//					.setCurrentGradientPainter(new StandardGradientPainter());
+//			SubstanceLookAndFeel.setCurrentTitlePainter(new FlatTitlePainter());
+//		} catch (Exception e) {
+//			System.err.println("Something went wrong!");
+//		}
+		Thread splash = new Thread(new Runnable() {
 			@Override
 			public void run() {
-				long begin_time=System.currentTimeMillis();
-				JFrame GFrame = new JFrame();	//创建窗口
-				SplashPanel DPanel = new SplashPanel();	//创建画板
-				/*设置JFrame*/
+				long begin_time = System.currentTimeMillis();
+				JFrame GFrame = new JFrame(); // 创建窗口
+				SplashPanel DPanel = new SplashPanel(); // 创建画板
+				/* 设置JFrame */
 				GFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				GFrame.setUndecorated(true);
-				Toolkit kit=Toolkit.getDefaultToolkit();
-				Dimension screensize=kit.getScreenSize();
-				int screenheight=screensize.height;
-				int screenwidth=screensize.width;
-				GFrame.setLocation(screenwidth/8,screenheight/8);
+				Toolkit kit = Toolkit.getDefaultToolkit();
+				Dimension screensize = kit.getScreenSize();
+				int screenheight = screensize.height;
+				int screenwidth = screensize.width;
+				GFrame.setLocation(screenwidth / 8, screenheight / 8);
 				GFrame.setSize(Config.UI_WIDTH, Config.UI_HEIGHT);
 				GFrame.setVisible(true);
-				GFrame.add(DPanel);	//在JFrame中加入DPanel
-				DPanel.launch();					
-				while(true){
-					if(System.currentTimeMillis()-begin_time>3000){
+				GFrame.add(DPanel); // 在JFrame中加入DPanel
+				DPanel.launch();
+				while (true) {
+					if (System.currentTimeMillis() - begin_time > 3000) {
 						GFrame.dispose();
 						break;
-					}				
-				}				
-			}			
+					}
+				}
+			}
 		});
-		
-		Thread mainUI=new Thread(new Runnable(){
+
+		Thread mainUI = new Thread(new Runnable() {
 
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
-				long begin_time=System.currentTimeMillis();
-				
+				long begin_time = System.currentTimeMillis();
+
 				MainFrame frame = new MainFrame();
 				frame.setLayout(null);
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				frame.setResizable(false);
-				//获取屏幕宽高
-				Toolkit kit=Toolkit.getDefaultToolkit();
-				Dimension screensize=kit.getScreenSize();
-				int screenheight=screensize.height;
-				int screenwidth=screensize.width;
-				
-				frame.setSize(Config.UI_WIDTH,Config.UI_HEIGHT);
-				frame.setLocation(screenwidth/8,screenheight/8);				
-				MainFrame.mainFrame.playerSelectionPanel.setBounds(0, Config.TOP_TAB_HEIGHT+Config.INTRODUCTION_WHITE, 
-			Config.UI_WIDTH, Config.SELECTION_HEIGHT);
+				// 获取屏幕宽高
+				Toolkit kit = Toolkit.getDefaultToolkit();
+				Dimension screensize = kit.getScreenSize();
+				int screenheight = screensize.height;
+				int screenwidth = screensize.width;
+
+				frame.setSize(Config.UI_WIDTH, Config.UI_HEIGHT);
+				frame.setLocation(screenwidth / 8, screenheight / 8);
+				MainFrame.mainFrame.playerSelectionPanel.setBounds(0,
+						Config.TOP_TAB_HEIGHT + Config.INTRODUCTION_WHITE,
+						Config.UI_WIDTH, Config.SELECTION_HEIGHT);
 				frame.setVisible(true);
-				while(true){
-					if((System.currentTimeMillis()-begin_time)>3000){
+				while (true) {
+					if ((System.currentTimeMillis() - begin_time) > 3000) {
 						frame.setVisible(true);
 						break;
 					}
 				}
-			}			
+			}
 		});
-		
+
 		mainUI.start();
 		splash.start();
 	}
