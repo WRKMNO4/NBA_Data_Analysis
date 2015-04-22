b263628be4e0b11ecd7cfd92fb70f80f0afe150c
Bagnogetoff <1051943988@qq.com>
2015-04-22 16:00:56 +0800

    .

--- src/com/kmno4/common/Config.java
+++ src/com/kmno4/common/Config.java
@@ -43,8 +43,8 @@ public class Config {
 	    MATCH_DETAIL_HEIGHT = 600;
 	
 	public static final int
-	    LASTEST_GAME_FRAME_UNIT_HEIGHT = 50,
-	    LASTEST_GAME_FRAME_WIDTH = 300;
+	    LASTEST_GAME_FRAME_UNIT_HEIGHT = 30,
+	    LASTEST_GAME_FRAME_WIDTH = 500;
 	
 	public static final int MAIN_FRAME_X=100;
 	public static final int MAIN_FRAME_Y=100;
