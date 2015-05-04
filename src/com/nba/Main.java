5a410866c753a67b34ab5b7a6ae4a9b61294434c
shengxiaoyu <shengxiaoyu>
2015-05-04 18:07:47 +0800

    ~~~

--- src/com/nba/Main.java
+++ src/com/nba/Main.java
@@ -5,10 +5,7 @@ import com.kmno4.common.Config;
 public class Main {
    
 	public static void main(String[] args){
-//		Console console = new Console() ;
-//		console.execute(System.out, args);
-		String[][] body = new String[2][13] ;
-		body[0] = Config.PLAYER_SORT_TOTAL ;
-		System.out.println(body[0][12]);
+		Console console = new Console() ;
+		console.execute(System.out, args);
     }
 }
