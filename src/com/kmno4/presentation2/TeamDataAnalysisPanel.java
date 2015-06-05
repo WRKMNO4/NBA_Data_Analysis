6a9321d4ae1310b2aa0e3e1faf74d15abf2cc31d
Bagnogetoff <1051943988@qq.com>
2015-06-05 17:02:34 +0800

    删除了某多余的模块

--- src/com/kmno4/presentation2/TeamDataAnalysisPanel.java
+++ src/com/kmno4/presentation2/TeamDataAnalysisPanel.java
@@ -13,9 +13,8 @@ import PO.TeamPO;
 /**
  * 球队单个分析界面
  * 内含球员分析{@link TeamPlayerAnalysisPanel}，
- * 球队全比赛分析{@link TeamMatchAnalysisPanel}，
  * 球队排名演变分析{@link TeamEvolutionAnalysisPanel}
- * 三个模块
+ * 两个模块
  * @author hutao
  *
  */
@@ -32,7 +31,6 @@ public class TeamDataAnalysisPanel extends JPanel {
 	private SelectPanel selectPanel;
 	
 	private JPanel current_panel;
-	private TeamMatchAnalysisPanel teamMatchAnalysisPanel;
 	private TeamPlayerAnalysisPanel teamPlayerAnalysisPanel;
 	private TeamEvolutionAnalysisPanel teamEvolutionAnalysisPanel;
 	private TeamPO teamPO;
@@ -60,24 +58,17 @@ public class TeamDataAnalysisPanel extends JPanel {
 	
 	
 	class SelectPanel extends JPanel {
-		JLabel player, match, evolu;
+		JLabel player, evolu;
 		public SelectPanel() {
 			setBounds(PADDING, PADDING + TEAM_LABEL_HEIGHT, Config.UI_WIDTH - PADDING * 2, SELECT_PART_HEIGHT);
 			setLayout(null);
 			player = new JLabel("球员分析", JLabel.CENTER);
 			player.setBounds(0, 0, SELECT_LABEL_WIDTH, SELECT_PART_HEIGHT);
-			match = new JLabel("比赛分析", JLabel.CENTER);
-			match.setBounds(SELECT_LABEL_WIDTH, 0, SELECT_LABEL_WIDTH, SELECT_PART_HEIGHT);
 			evolu = new JLabel("排名分析", JLabel.CENTER);
-			evolu.setBounds(SELECT_LABEL_WIDTH * 2, 0, SELECT_LABEL_WIDTH, SELECT_PART_HEIGHT);
+			evolu.setBounds(SELECT_LABEL_WIDTH, 0, SELECT_LABEL_WIDTH, SELECT_PART_HEIGHT);
 			player.addMouseListener(new LMouseAdapter(teamDataAnalysisFrame) {
 				public void mouseClicked(MouseEvent e) {
 					toPlayerAnalysis();
-				}
-			});
-			match.addMouseListener(new LMouseAdapter(teamDataAnalysisFrame) {
-				public void mouseClicked(MouseEvent e) {
-					toMatchAnalysis();
 				}
 			});
 			evolu.addMouseListener(new LMouseAdapter(teamDataAnalysisFrame) {
@@ -86,20 +77,11 @@ public class TeamDataAnalysisPanel extends JPanel {
 				}
 			});
 			add(player);
-			add(match);
 			add(evolu);
 			
 		}
 	}
 	
-	/**
-	 * 切换到球队比赛部分
-	 */
-	private void toMatchAnalysis() {
-		remove(current_panel);
-		add(current_panel = (teamMatchAnalysisPanel = new TeamMatchAnalysisPanel(teamPO, teamDataAnalysisFrame)));
-		teamDataAnalysisFrame.repaint();
-	}
 	/**
 	 * 切换到球队球员分析
 	 */
