7f2eb6a8a811ee58a7a0cabb37f5964012d6a411
Bagnogetoff <1051943988@qq.com>
2015-06-06 20:00:29 +0800

    0，0

--- src/com/kmno4/presentation2/AllTeamDataAnalysisPanel.java
+++ src/com/kmno4/presentation2/AllTeamDataAnalysisPanel.java
@@ -1,5 +1,8 @@
 package com.kmno4.presentation2;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JLabel;
@@ -22,9 +25,9 @@ public class AllTeamDataAnalysisPanel extends JPanel {
 	public static final int
 	    PADDING = 10,
 	    LABEL_HEIGHT = 60,
-	    SELECT_PANEL_HEIGHT = 60,
+	    SELECT_PANEL_HEIGHT = 45,
 	    PANEL_HEIGHT = Config.UI_HEIGHT - 2 * PADDING - LABEL_HEIGHT - SELECT_PANEL_HEIGHT,
-	    SELECT_LABEL_WIDTH = 100;
+	    SELECT_LABEL_WIDTH = 120;
 	
 	private JLabel somthing;
 	private SelectPanel selectPanel;
@@ -41,6 +44,8 @@ public class AllTeamDataAnalysisPanel extends JPanel {
 		
 		somthing = new JLabel("全球队数据分析", JLabel.LEFT);
 		somthing.setBounds(PADDING, PADDING, Config.UI_WIDTH - 2 * PADDING, LABEL_HEIGHT);
+		somthing.setForeground(Color.white);
+		somthing.setFont(new Font("default", 2, 30));
 		add(somthing);
 		selectPanel = new SelectPanel();
 		add(selectPanel);
@@ -56,12 +61,19 @@ public class AllTeamDataAnalysisPanel extends JPanel {
 			setBounds(PADDING, PADDING + LABEL_HEIGHT,
 					Config.UI_WIDTH - 2 * PADDING, SELECT_PANEL_HEIGHT);
 			setLayout(null);
+			setBackground(new Color(20, 79, 139, 150));
 			rank = new JLabel("排名数据分析", JLabel.LEFT);
 			rank.setBounds(0, 0, SELECT_LABEL_WIDTH, SELECT_PANEL_HEIGHT);
+			rank.setForeground(Color.white);
+			rank.setFont(new Font("default", 0, 15));
 			recent = new JLabel("最近10场分析", JLabel.LEFT);
 			recent.setBounds(SELECT_LABEL_WIDTH, 0, SELECT_LABEL_WIDTH, SELECT_PANEL_HEIGHT);
+			recent.setForeground(Color.white);
+			recent.setFont(new Font("default", 0, 15));
 			offen = new JLabel("进攻防守分析", JLabel.LEFT);
 			offen.setBounds(SELECT_LABEL_WIDTH * 2, 0, SELECT_LABEL_WIDTH, SELECT_PANEL_HEIGHT);
+			offen.setForeground(Color.white);
+			offen.setFont(new Font("default", 0, 15));
 			rank.addMouseListener(new LMouseAdapter(allTeamDataAnalysisFrame) {
 				public void mouseClicked(MouseEvent e) {
 					toRanking();
@@ -107,7 +119,10 @@ public class AllTeamDataAnalysisPanel extends JPanel {
 		add(currentPanel = (allTeamOffenAnalysisPanel = new AllTeamOffenAnalysisPanel(allTeamDataAnalysisFrame)));
 		allTeamDataAnalysisFrame.repaint();
 	}
-	
+	@Override
+	protected void paintComponent(Graphics g) {
+		g.drawImage(Config.DETAIL_BG.getImage(), 0, 0, this.getWidth(), this.getHeight(), null);
+	}
 	public static void main(String[] args) {
 		new AllTeamDataAnalysisFrame();
 	}
