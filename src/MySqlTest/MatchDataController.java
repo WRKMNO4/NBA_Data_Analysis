7ef215c6d94a89eb98a5d7e8f7a674c34f3d6cd8
shengxiaoyu <825363973@qq.com>
2015-06-10 22:18:21 +0800

    improve insert into matches

--- src/MySqlTest/MatchDataController.java
+++ src/MySqlTest/MatchDataController.java
@@ -2,10 +2,14 @@ package MySqlTest;
 
 import java.io.File;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+
+import org.python.antlr.base.stmt;
 
 import DataService.FileHelper.FileHelper;
 
@@ -14,11 +18,14 @@ public class MatchDataController {
 	java.sql.Connection con = null ;
 	String url = "jdbc:mysql://localhost:3306/NBA_DATA?"
             + "user=root&password=941104&useUnicode=true&characterEncoding=UTF8";
-	java.sql.Statement stmt = null ;
+	java.sql.PreparedStatement stmtMatch = null ;
+	PreparedStatement stmtScore = null ;
+	PreparedStatement stmtPlayerData = null ;
 	String stringOfMatches = "insert into matches(matchID,time,firstTeam,secondTeam,firstScore,secondScore)values(?,?,?,?,?,?)" ;
-	String stringOfScores = "insert into scoresOfOneMatch(matchID,scoreID,firstScore,secondScore) values" ;
+	String stringOfScores = "insert into scoresOfOneMatch(matchID,firstScore,secondScore) values(?,?,?)" ;
 	String stringOfPlayerDataOfOneMatch = "insert into playerDataOfOneMatch(matchID,team,name,position,ifStarting,persentTime,numberOfShooting,numberOfShotAttempt,numberOf3_point,numberOf3_pointAttempt"
-			+ ",numberOfFreeThrow,numberOfFreeThrowAttempt,numberOfAttackRebound,numberOfDefenseRebound,numberOfRebound,numberOfAssist,numberOfSteal,numberOfBlock,numberOfFault,numberOfFoul,score)values" ;
+			+ ",numberOfFreeThrow,numberOfFreeThrowAttempt,numberOfAttackRebound,numberOfDefenseRebound,numberOfRebound,numberOfAssist,numberOfSteal,numberOfBlock,numberOfFault,numberOfFoul,score)values"
+			+ "(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)" ;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MatchDataController ml = new MatchDataController() ;
@@ -34,8 +41,11 @@ public class MatchDataController {
 			
 	    	con = DriverManager.getConnection(url);
 	    	System.out.println("链接数据库");
+	    	con.setAutoCommit(false) ;
 	    	
-	    	stmt = con.createStatement() ;
+	    	stmtMatch = con.prepareStatement(stringOfMatches,ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_READ_ONLY) ;
+	    	stmtScore = con.prepareStatement(stringOfScores,ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_READ_ONLY) ;
+	    	stmtPlayerData = con.prepareStatement(stringOfPlayerDataOfOneMatch,ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_READ_ONLY) ;
 	    }
 	    catch (SQLException e) {
 				// TODO Auto-generated catch block
@@ -54,93 +64,90 @@ public class MatchDataController {
 			File[] allFiles = file.listFiles() ;
 			Arrays.sort(allFiles,new CompareByTime());
 			
-			for(int i = 0;i<allFiles.length;i++){
-				ArrayList<String> tempString = FileHelper.readByLine(allFiles[i]) ;
-				
-				int id = i ;
-				
-				//match
-				String date = getTheTime(allFiles[i].getName()) ;
-				String[] strs = tempString.get(0).split(";") ;
-				String[] teams = strs[1].split("-") ;
-				String[] scoresOfString = strs[2].split("-") ;
-				int[] scores = {Integer.parseInt(scoresOfString[0]),Integer.parseInt(scoresOfString[1])} ;
-				String insertOfMatches = stringOfMatches + "("+id+",'"+date+"','"+teams[0]+"','"+teams[1]+"',"+scores[0]+","+scores[1]+")" ;
-				System.out.println(insertOfMatches);
-				try {
-					stmt.executeUpdate(insertOfMatches) ;
-					System.out.println("插入比赛");
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					System.out.println("比赛插入失败");
-					e.printStackTrace();
-				}
-				
-				//scroes
-				strs = tempString.get(1).split(";") ;
-				for(int j = 0;j<strs.length;j++){
-					String s = strs[j] ;
-					scoresOfString = s.split("-") ;
-				    int[] scores2 = {Integer.parseInt(scoresOfString[0]),Integer.parseInt(scoresOfString[1])} ;
-				    String insertOfScores = stringOfScores+"("+id+","+j+","+scores2[0]+","+scores2[1]+")" ;
-				    System.out.println(insertOfScores);
-				    try {
-						stmt.executeUpdate(insertOfScores) ;
-						System.out.println("插入分数");
-					} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						System.out.println("插入分数失败");
-						e.printStackTrace();
-					}
-				}
-				
-				
-				//playerData
-				String team = teams[0] ;
-				int ifStarting = 5 ;
-				for(int j1 = 3;j1<tempString.size();j1++){
-					String[] onePlayer = tempString.get(j1).split(";") ;
-					if(onePlayer.length<2){
-						team = teams[1] ;
-						ifStarting = 5 ;
-						continue ;
-					}
-					ArrayList<String> dataOfStr = new ArrayList<>() ;
-					ArrayList<Integer> dataOfInt = new ArrayList<>() ;
+			
+			int count = 1000 ;
+		
+			try {
+				for(int i =0;i<allFiles.length;i++){
+					ArrayList<String> tempString = FileHelper.readByLine(allFiles[i]) ;
+					String firstLine = tempString.get(0) ;
+					String[] strs = firstLine.split(";") ;
+					String[] scores = strs[2].split("-") ;
+					stmtMatch.setInt(1, i) ;
+					stmtMatch.setString(2, getTheTime(allFiles[i].getName()));
+					stmtMatch.setString(3,strs[1].substring(0,3));
+					stmtMatch.setString(4, strs[1].substring(4));
+					stmtMatch.setInt(5, Integer.parseInt(scores[0]));
+					stmtMatch.setInt(6, Integer.parseInt(scores[1]));
+					stmtMatch.addBatch();
+					count-- ;
 					
-					dataOfInt.add(id) ;
-					if(ifStarting>0)
-				    	dataOfInt.add(1) ;
-					else
-						dataOfInt.add(0) ;
-					for(int k = 3;k<onePlayer.length;k++){
-						try{
-							dataOfInt.add(Integer.parseInt(onePlayer[k])) ;
-						}catch(Exception e){
-							if(k==onePlayer.length)
-								dataOfInt.add(2*dataOfInt.get(2)+dataOfInt.get(4)+dataOfInt.get(6)) ;
-							dataOfInt.add(0) ;
+					String secondLine = tempString.get(1) ;
+					scores =secondLine.split(";") ;
+					for(String str :scores){
+						String[] s = str.split("-") ;
+						stmtScore.setInt(1, i);
+						stmtScore.setInt(2, Integer.parseInt(s[0]));
+						stmtScore.setInt(3,Integer.parseInt(s[1]));
+						stmtScore.addBatch();
+						count-- ;
+					}
+					
+					String team = tempString.get(2) ;
+					int ifStarting = 5 ;
+					for(int j=3;j<tempString.size();j++){
+						String[] onePlayer = tempString.get(j).split(";") ;
+						if(onePlayer.length<2){
+							team = tempString.get(j) ;
+							ifStarting = 5 ;
+							continue ;
 						}
+						stmtPlayerData.setInt(1, i);
+						stmtPlayerData.setString(2, team);
+						stmtPlayerData.setString(3, checkString(onePlayer[0]));
+						stmtPlayerData.setString(4, onePlayer[1]);
+						stmtPlayerData.setInt(5, (ifStarting>0)?1:0);
+						stmtPlayerData.setString(6, onePlayer[2]);
+						stmtPlayerData.setInt(7,checkInt(onePlayer[3]));
+						stmtPlayerData.setInt(8, checkInt(onePlayer[4]));
+						stmtPlayerData.setInt(9, checkInt(onePlayer[5]));
+						stmtPlayerData.setInt(10, checkInt(onePlayer[6]));
+						stmtPlayerData.setInt(11, checkInt(onePlayer[7]));
+						stmtPlayerData.setInt(12, checkInt(onePlayer[8]));
+						stmtPlayerData.setInt(13, checkInt(onePlayer[9]));
+						stmtPlayerData.setInt(14, checkInt(onePlayer[10]));
+						stmtPlayerData.setInt(15, checkInt(onePlayer[11]));
+						stmtPlayerData.setInt(16, checkInt(onePlayer[12]));
+						stmtPlayerData.setInt(17, checkInt(onePlayer[13]));
+						stmtPlayerData.setInt(18, checkInt(onePlayer[14]));
+						stmtPlayerData.setInt(19, checkInt(onePlayer[15]));
+						stmtPlayerData.setInt(20, checkInt(onePlayer[16]));
+						try{
+							stmtPlayerData.setInt(21, Integer.parseInt(onePlayer[17]));
+						}catch(NumberFormatException e){
+							int goal = Integer.parseInt(onePlayer[3])*2+Integer.parseInt(onePlayer[5])+Integer.parseInt(onePlayer[7]) ;
+							stmtPlayerData.setInt(21, goal);
+						}
+						stmtPlayerData.addBatch();
+						count-- ;
+						
+						
 					}
-					
-					dataOfStr.add(team) ;
-					dataOfStr.add(checkString(onePlayer[0])) ;
-					dataOfStr.add(onePlayer[1]) ;
-					dataOfStr.add(onePlayer[2]) ;
-					
-					String insertPlayerData = stringOfPlayerDataOfOneMatch +"("+dataOfInt.get(0)+",'"+dataOfStr.get(0)+"','"+dataOfStr.get(1)+"','"+dataOfStr.get(2)+"',"+dataOfInt.get(1)+",'"+dataOfStr.get(3)+"',"+
-					dataOfInt.get(2)+","+dataOfInt.get(3)+","+dataOfInt.get(4)+","+dataOfInt.get(5)+","+dataOfInt.get(6)+","+dataOfInt.get(7)+","+dataOfInt.get(8)+","+dataOfInt.get(9)+","+dataOfInt.get(10)+","+dataOfInt.get(11)+","+
-					dataOfInt.get(12)+","+dataOfInt.get(13)+","+dataOfInt.get(14)+","+dataOfInt.get(15)+","+dataOfInt.get(16)+")" ;
-					System.out.println(insertPlayerData);
-					try {
-						stmt.executeUpdate(insertPlayerData) ;
-						System.out.println("插入球员信息");
-					} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-						System.out.println("插入球员信息失败");
+					if(count<0){
+						stmtMatch.executeBatch() ;
+						stmtScore.executeBatch() ;
+						stmtPlayerData.executeBatch() ;
+						con.commit();
 					}
 				}
+				stmtMatch.executeBatch() ;
+				stmtScore.executeBatch() ;
+				stmtPlayerData.executeBatch() ;
+				con.commit();
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
 	}
@@ -176,5 +183,12 @@ public class MatchDataController {
 			string = string.substring(0,i)+"\\"+string.substring(i);
 		}
 		return string ;
+	}
+	private int checkInt(String number){
+		try{
+			return Integer.parseInt(number) ;
+		}catch(Exception e){
+			return 0 ;
+		}
 	}
 }
